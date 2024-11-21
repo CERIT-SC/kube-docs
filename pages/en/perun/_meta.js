ac16c9d3114d423be538d88f67e231792b129ca1
Milan <milan.danecek@gmail.com>
2024-11-21 14:58:05 +0100

    Du docs add perun docs

--- pages/en/perun/_meta.js
+++ pages/en/perun/_meta.js
@@ -1,4 +1,11 @@
 export default {
 	"user-management": "User Management",
-	"service-account": "Service account handling"
+	"sending-vo-invitation": "Sending VO invitation",
+	"user-approval": "User Approval",
+	"extension-of-membership-in-vo": "Extension of mebership in VO",
+	"adding-vo-manager-into-vo": "Adding VO manager into VO",
+	"managing-service-user-account": "Managing service user account",
+	"changing-service-account-password": "Changing service account password",
+	"creating-group": "Creating group",
+	"adding-ssh-key": "Adding SSH key"
 }
