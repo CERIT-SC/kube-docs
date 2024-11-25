556bbf57aab87ae8ee25afc0e8a4cd1a29d1da47
Milan <milan.danecek@gmail.com>
2024-11-25 17:34:42 +0100

    Du docs add perun service-user #1

--- pages/en/perun/_meta.js
+++ pages/en/perun/_meta.js
@@ -5,7 +5,5 @@ export default {
 	"extension-of-membership-in-vo": "Extension of mebership in VO",
 	"adding-vo-manager-into-vo": "Adding VO manager into VO",
 	"managing-service-user-account": "Managing service user account",
-	"changing-service-account-password": "Changing service account password",
 	"creating-group": "Creating group",
-	"adding-ssh-key": "Adding SSH key"
 }
