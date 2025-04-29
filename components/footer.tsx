608d779b4416c6a6115eee2773bfe4d8806a884c
xhejtman <xhejtman@gmail.com>
2025-04-30 00:51:47 +0200

    Create footer.tsx

---
+++ components/footer.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+
+export function Footer() {
+   return (
+     <footer className="mt-auto border-t py-12 text-fd-secondary-foreground ">
+     <div className="container flex flex-col items-center gap-4 sm:flex-row sm:items-end sm:justify-between --color-fd-background relative z-20">
+     </div>
+     </footer>
+   );
+}
